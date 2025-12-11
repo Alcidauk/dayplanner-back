5541be3b686aa897897425577fd015216b246146
Aksel <axel.akondegmail.com>
2025-12-11 18:18:13 +0100

    create tables from models in database

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-
+from app.database.database import engine, Base
 from app.routers import user
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(user.router, prefix="/user", tags=["User"])
 
